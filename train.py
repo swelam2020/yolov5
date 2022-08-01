5263d34a5db8cd3e5673d8b706b1b414ff913814
muhammad swelam <93865477+swelam2020@users.noreply.github.com>
2022-08-01 03:07:20 -0700

    Update train.py

--- train.py
+++ train.py
@@ -268,7 +268,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
     last_opt_step = -1
     maps = np.zeros(nc)  # mAP per class
     results = (0, 0, 0, 0, 0, 0, 0)  # P, R, mAP@.5, mAP@.5-.95, val_loss(box, obj, cls)
-    Run.log('metric-name', results)
+    run.log('metric-name', results)
     scheduler.last_epoch = start_epoch - 1  # do not move
     scaler = torch.cuda.amp.GradScaler(enabled=amp)
     stopper, stop = EarlyStopping(patience=opt.patience), False
